f863924926b64e57ecbe5a63e3adf2d179abf11c
Prodip Roy <you@example.com>
2023-05-08 19:37:03 +0600

    Higher order function

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -24,14 +24,22 @@
  * - Hoisitng
  */
 
-function generateTwoRandomNumber(max, cb){
+// Higher Order Function
+function generateTwoRandomNumber(max, cb) {
     const random1 = Math.floor(Math.random() * max); //const
     const random2 = Math.floor(Math.random() * max); //const
     const result = cb(random1, random2);
     return result;
 }
 
-generateTwoRandomNumber((rand1, rand2) =>{
+generateTwoRandomNumber(100, (rand1, rand2) => {
     console.log(rand1, rand2);
 })
+
+console.log(generateTwoRandomNumber(1000, (rand1, rand2) => rand1 + rand2));
+console.log(generateTwoRandomNumber(10, (rand1, rand2) => rand1 * rand2));
+console.log(
+    generateTwoRandomNumber(1000, (
+        rand1, rand2) => rand1 * rand2 + rand2 * rand1
+    ));
 
