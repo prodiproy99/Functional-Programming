61f2a5c39c63e8d55d6e7b87cb49e27363860ad3
Prodip Roy <you@example.com>
2023-05-08 16:51:42 +0600

    Function Concepts

---
+++ Functional Programming/app.js
@@ -0,0 +1,24 @@
+// // Pure function
+// function sum(a, b){
+//     return a+b;
+// }
+
+// sum(2, 3)
+
+// // impure function
+// function log(msg){
+//     console.log(msg);
+// }
+
+/**
+ * Higher order function
+ * - function can be passed as an argument
+ * - function can be return from another function
+ */
+
+/**
+ * Hidden concepts
+ * - Scope
+ * - Closure
+ * - Function Context
+ */
