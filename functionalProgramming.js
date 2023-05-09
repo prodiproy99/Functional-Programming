a6ac4ada723d28152ea46bd184980092ad89e51c
Prodip Roy <you@example.com>
2023-05-09 08:01:24 +0600

    function can be return from another function

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -25,21 +25,48 @@
  */
 
 // Higher Order Function
-function generateTwoRandomNumber(max, cb) {
-    const random1 = Math.floor(Math.random() * max); //const
-    const random2 = Math.floor(Math.random() * max); //const
-    const result = cb(random1, random2);
-    return result;
+// function generateTwoRandomNumber(max, cb) {
+//     const random1 = Math.floor(Math.random() * max); //const
+//     const random2 = Math.floor(Math.random() * max); //const
+//     const result = cb(random1, random2);
+//     return result;
+// }
+
+// // const cb = function(rand1, rand2){
+// //     console.log(rand1, rand2);
+// // }
+
+// generateTwoRandomNumber(100, (rand1, rand2) => {
+//     console.log(rand1, rand2);
+// })
+
+// console.log(generateTwoRandomNumber(1000, (rand1, rand2) => rand1 + rand2));
+// console.log(generateTwoRandomNumber(10, (rand1, rand2) => rand1 * rand2));
+// console.log(
+//     generateTwoRandomNumber(1000, (
+//         rand1, rand2) => rand1 * rand2 + rand2 * rand1
+//     ));
+
+
+function power(p) {
+    return function (n) {
+        let result = 1;
+        for (let i = 1; i <= p; i++) {
+            result *= n;
+        } 
+        return result;
+    }
 }
 
-generateTwoRandomNumber(100, (rand1, rand2) => {
-    console.log(rand1, rand2);
-})
+const sqr = power(2)
+const cube = power(3)
+const power8 = power(8)
 
-console.log(generateTwoRandomNumber(1000, (rand1, rand2) => rand1 + rand2));
-console.log(generateTwoRandomNumber(10, (rand1, rand2) => rand1 * rand2));
-console.log(
-    generateTwoRandomNumber(1000, (
-        rand1, rand2) => rand1 * rand2 + rand2 * rand1
-    ));
+console.log('SQR', sqr);
+console.log('Cube', cube);
+console.log('power8', power8);
+
+console.log(power8(3));
+console.log(power8(2));
+console.log(power8(4));
 
